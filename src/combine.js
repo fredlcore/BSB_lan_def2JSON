e1139a9d9736615e9244e373cb018034c1b1584e
Konrad Mattheis <konne@users.noreply.github.com>
2020-12-13 16:33:51 +0100

    * move categories in json

--- src/combine.js
+++ src/combine.js
@@ -34,19 +34,80 @@ let combined = {};
 
 var entries = fs.readdirSync('./');
 
+var comArray = [];
+
 for (i = 0; i < entries.length; i++) {
   let item = entries[i];
 
-  if (item.toLocaleLowerCase().endsWith('.json')) {
+  if (item.toLocaleLowerCase().endsWith('.json') && !item.toLocaleLowerCase().endsWith('all.json')) {
     let lang = JSON.parse(fs.readFileSync(item));
-
     combined = mergeDeep(combined, lang);
+
   }
 }
+
+console.log("cleanup");
+
+function cleanUpLang(input) {
+  let de = input['DE'];
+  let en = input['EN'];
+
+  if (de == en)
+    delete input['EN'];
+
+  for (let key in input) {
+    if (key != "DE" && key != "EN") {
+      if (de == input[key] || en == input[key])
+        delete input[key];
+    }
+  }
+  return input;
+}
+
+let categories = {};
+for (let key in combined.commands) {
+  let item = combined.commands[key];
+
+  if (!item.category)
+    continue;
+
+  let categorie = categories[item.category.min];
+  if (!categorie) {
+    if (item.category && item.category.name)
+      item.category.name = cleanUpLang(item.category.name);
+
+    categories[item.category.min] = item.category;
+    categorie = item.category;
+    categorie.commands = [];
+  }
+  delete item.category;
+
+  if (item.description)
+    item.description = cleanUpLang(item.description);
+
+  if (item.enum) {
+    for (let enumKey in item.enum) {
+      item.enum[enumKey] = cleanUpLang(item.enum[enumKey]);
+    }
+  }
+
+  if (item.flags) {
+    item.flags = item.flags.sort();
+
+    let idx = item.flags.lastIndexOf('')
+    if (idx > -1)
+      item.flags = item.flags.slice(idx + 1);
+  }
+  if (item.dummy !== undefined)
+    delete item.dummy;
+  categorie.commands.push(item);
+}
+combined.commands = categories;
 
 // walk through the tree and fix the tree
 // idx: object -> array
 // remove "" flags
 // combine lang if missing
 
+fs.writeFileSync('small_all.json', JSON.stringify(combined));
 fs.writeFileSync('all.json', JSON.stringify(combined, null, 1));
